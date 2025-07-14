1a7f1b7dfe676279b84f15e9cdd7bc818464f2b0
gayantharaka <a.k.g.t.damayantha@gmail.com>
2025-07-14 20:10:52 +0530

    Added implementation for About page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './core/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/chat',pathMatch:'full'},
+  {path:'about',component:AboutComponent},
+ // {path:'chat',loadChildren:()=>import('./chat/chat.module').then(m=>m.ChatModule))}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
